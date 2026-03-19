095342bb13daea399e1c708459002ad4683fab4a
Ammar Zahher <ammaryasser1938@gmail.com>
2026-03-19 20:32:41 +0300

    Create Using Python to Access Web Data.py

---
+++ Using Python to Access Web Data.py
@@ -0,0 +1,24 @@
+import urllib.error, urllib.request, urllib.parse
+
+j = dict()
+link = input("add link: ")
+b = urllib.request.urlopen(link)
+
+for line in b:
+    m = line.decode().strip()
+    n = m.split()
+    for k in n:
+        j[k] = j.get(k, 0) + 1
+
+o = j.items()
+mh = None # the most common word
+jk = None # the name of the most repeated word
+
+# a is a word
+# q is a count
+for a, q in o:
+    if jk is None or q > jk:
+        jk = q
+        mh = a
+
+print(mh, jk)
